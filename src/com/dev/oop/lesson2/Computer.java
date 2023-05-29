a86abb8ba1b28dfdc16741be1899795922d51f08
VladimirImir <mimimvvv@gmail.com>
2023-05-30 01:38:43 +0300

    3.1

--- src/com/dev/oop/lesson2/Computer.java
+++ src/com/dev/oop/lesson2/Computer.java
@@ -5,6 +5,23 @@ public class Computer {
     int ssd = 512;
     int ram = 1024;
 
+    /** Коструктор по умолчанию. */
+    Computer(){
+        System.out.println("Я был создан!");
+    }
+
+    /** Конструктор с параметром. */
+    Computer(int newSsd){
+        ssd = newSsd;
+    }
+
+    /** Конструктор с параметрами. */
+    Computer(int ssd, int newRam){
+        /** this - решает проблему конфликта имен. */
+        this.ssd = ssd;
+        ram = newRam;
+    }
+
     void load(){
         System.out.println("Я загрузился!");
     }
@@ -12,5 +29,6 @@ public class Computer {
     void printState(){
         System.out.println("Ssd: " + ssd);
         System.out.println("Ram: " + ram);
+        System.out.println();
     }
 }
--- src/com/dev/oop/lesson2/ComputerRunner.java
+++ src/com/dev/oop/lesson2/ComputerRunner.java
@@ -3,17 +3,26 @@ package com.dev.oop.lesson2;
 public class ComputerRunner {
 
     public static void main(String[] args) {
-        int value = 5;
-        Computer computer = new Computer();
-        
-        System.out.println("-------");
-        computer.load();
 
+        /** Ctrl + Alt + V - создать переменную. */
+
+        Computer computer = new Computer();
+        computer.printState();
+
+        Computer computer2 = new Computer(2048);
+        computer2.printState();
+
+        Computer computer3 = new Computer(4096, 4096);
+        computer3.printState();
+
+        /*System.out.println("-------");
+        computer.load();
+        
         System.out.println("-------");
         System.out.println("Ram: " + computer.ram);
         System.out.println("Ssd: " + computer.ssd);
-
+        
         System.out.println("-------");
-        computer.printState();
+        computer.printState();*/
     }
 }
